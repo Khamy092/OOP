50a72b4d8a7ce7a0630ada016bd381b62750e716
khamy092 <khamy092@mymail.unisa.edu.au>
2023-01-20 10:32:57 +1030

    inheritence

--- practice5.py
+++ practice5.py
@@ -3,27 +3,49 @@
 
 
 class Driver:
-    def __init__(self, name, age):
+    def __init__(self, name, age, car):
         self.name = name
         self.age = age
+        self.car = car
 
     def drive(self):
         print(self.name, "is driving")
 
+    def vehicle(self):
+        print(self.name, "is driving a", self.car)
+
 class Rider:
-    def __init__(self, name, age):
+    def __init__(self, name, age, motorcycle):
         self.name = name
         self.age = age
+        self.motorcycle = motorcycle
 
     def ride(self):
         print(self.name, "is riding")
 
+    def vehicle(self):
+        print(self.name, "is riding a", self.motorcycle)
+
 class Uber(Driver, Rider):
-    def __init__(self, name, age):
-        Driver.__init__(self, name, age)
-        Rider.__init__(self, name, age)
+    def __init__(self, name, age, car, motorcycle):
+        Driver.__init__(self, name, age, car)
+        Rider.__init__(self, name, age, motorcycle)
+        Driver.car = car
+        Rider.motorcycle = motorcycle
 
     def pay(self):
         print(self.name, "is paying")
+
+
+john0101 = Uber("John", 25, "Toyota", "Honda")
+john0101.drive()
+john0101.ride()
+john0101.pay()
+john0101.vehicle()
+john0101.motorcycle()
+
+
+
+
 
 
