8181f0e741e75f7a4f79600698cefe2b62e04d5d
khamy092 <khamy092@mymail.unisa.edu.au>
2023-01-21 15:11:38 +1030

    partially working

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -25,3 +25,40 @@ class Calculator:
     
     def power(self):
         return self.num1 ** self.num2 
+
+
+
+def ask_User():
+    print("Welcome to the simple calculator!")
+    print("Please enter the first number: ")
+    num1 = int(input())
+    print("Please enter the second number: ")
+    num2 = int(input())
+    print("Please enter the operation you would like to perform: ")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Power")
+    operation = int(input())
+    return num1, num2, operation
+
+
+
+ask_User()
+num1, num2, operation = ask_User()
+calc = Calculator(num1, num2)
+if operation == 1:
+    print(calc.add(num1, num2))
+elif operation == 2:
+    print(calc.subtract(num1, num2))
+elif operation == 3:
+    print(calc.multiply(num1, num2))
+elif operation == 4:
+    print(calc.divide(num1, num2))
+elif operation == 5:
+    print(calc.power(num1, num2))
+else:
+    print("Invalid operation")
+
+
